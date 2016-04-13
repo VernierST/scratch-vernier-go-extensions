21a5c064d65402ab934d960a01215d0caa5e2df2
Kreg Hanning <khanning@gmail.com>
2016-04-13 00:16:39 -0400

    Read device calibration record

--- gotempExtension.js
+++ gotempExtension.js
@@ -13,44 +13,119 @@
   var CMD_INIT_RESPONSE = 0x9a,
     CMD_RESPONSE = 0x5a,
     CMD_MEASUREMENT_RESPONSE = 0x01,
-    CMD_READ_MEMORY_RESPONSE = 0x4f;
+    CMD_READ_MEMORY_RESPONSE = 0x4f,
+    CMD_READ_FINAL_MEMORY_RESPONSE = 0x52;
 
-  function processData(inputData) {
-    var data = new Uint8Array(inputData);
+  var initReceived = false;
+  var calibrationRead = false;
+  var ledSet = false;
+  var periodSet = false;
+  var memRead = new Uint8Array(16);
 
-    switch (data[0]) {
-      case CMD_MEASUREMENT_RESPONSE:
-        var measurement = (data[3] << 8) | (data[2] & 0xFF);
-        var volts = ((2.5 / 32768) * measurement) + 2.5;
-        var reading = (102.4 * volts) - 257.812988281;
-        currentTemp = parseFloat(Math.round(reading * 100) / 100).toFixed(2);
-        break;
-      case CMD_INIT_RESPONSE:
-        console.log('Received init response');
+  var intercept = 0;
+  var slope = 0;
+  
+  function initializeDevice(inputData) {
+
+    if (!initReceived) {
+      if (inputData[0] === CMD_INIT_RESPONSE) {
+        console.log("Go!Temp initialized");
+        initReceived = true;
+        var out = [0, CMD_READ_MEMORY, 0x46, 0x8, 0, 0, 0, 0];
+        device.write(new Uint8Array(out).buffer);
+      } else {
+        var initCmd = [0, CMD_INIT, 0, 0, 0, 0, 0, 0];
+        device.write(new Uint8Array(initCmd).buffer);
+      }
+      return;
+    }
+
+    if (!calibrationRead) {
+      if (inputData[0] === CMD_READ_MEMORY_RESPONSE) {
+        memRead.set(inputData, 0);
+      } else if (inputData[0] === CMD_READ_FINAL_MEMORY_RESPONSE) {
+        if (memRead[0] != CMD_READ_MEMORY_RESPONSE) {
+          var out = [0, CMD_READ_MEMORY, 0x46, 0x8, 0, 0, 0, 0];
+          device.write(new Uint8Array(out).buffer);
+          return;
+        }
+        memRead.set(inputData, 8);
+        intercept = IEEEtoFloat([memRead[2], memRead[3], memRead[4], memRead[5]]);
+        slope = IEEEtoFloat([memRead[6], memRead[7], memRead[9], memRead[10]]);
+        console.log("Go!Temp calibration read");
+        calibrationRead = true;
         var out = [0, CMD_SET_LED_STATE, 0x80, 0x10, 0, 0, 0, 0];
         device.write(new Uint8Array(out).buffer);
-        break;
-      case CMD_RESPONSE:
-        switch (data[1]) {
-          case CMD_SET_LED_STATE:
-            console.log('Received LED state response');
-            var out = [0, CMD_SET_MEASUREMENT_PERIOD, 0x43, 0x0F, 0, 0, 0, 0];
-            device.write(new Uint8Array(out).buffer);
-            break;
-          case CMD_SET_MEASUREMENT_PERIOD:
-            console.log('Received measurement period response');
-            var out = [0, CMD_START_MEASUREMENTS, 0, 0, 0, 0, 0, 0];
-            device.write(new Uint8Array(out).buffer);
-            break;
-          case CMD_START_MEASUREMENTS:
-            break;
-        }  
-        break;
-      default:
-        console.log("Unknown command:");
-        console.log(data);
-        break;
+      } else {
+        var out = [0, CMD_READ_MEMORY, 0x46, 0x8, 0, 0, 0, 0];
+        device.write(new Uint8Array(out).buffer);
+      }
+      return;
     }
+
+    if (!ledSet) {
+      if (inputData[1] === CMD_SET_LED_STATE) {
+        console.log("Go!Temp LED is on");
+        var out = [0, CMD_SET_MEASUREMENT_PERIOD, 0x43, 0x0F, 0, 0, 0, 0];
+        device.write(new Uint8Array(out).buffer);
+        ledSet = true;
+      } else {
+        var out = [0, CMD_SET_LED_STATE, 0x80, 0x10, 0, 0, 0, 0];
+        device.write(new Uint8Array(out).buffer);
+      }
+      return;
+    }
+
+    if (!periodSet) {
+      if (inputData[1] === CMD_SET_MEASUREMENT_PERIOD) {
+        console.log("Go!Temp measurement period set");
+        var out = [0, CMD_START_MEASUREMENTS, 0, 0, 0, 0, 0, 0];
+        device.write(new Uint8Array(out).buffer);
+        periodSet = true;
+
+        clearInterval(poller);
+        poller = setInterval(function() {
+          device.read(function(rawData) {
+            var data = new Uint8Array(rawData);
+            if (data.length > 0)
+              processInput(data);
+          });
+        }, 50);
+
+      } else {
+        var out = [0, CMD_SET_MEASUREMENT_PERIOD, 0x43, 0x0F, 0, 0, 0, 0];
+        device.write(new Uint8Array(out).buffer);
+      }
+      return;
+    }
+  }
+
+  function processInput(inputData) {
+    if (inputData[0] === CMD_MEASUREMENT_RESPONSE) {
+      var measurement = (inputData[3] << 8) | (inputData[2] & 0xFF);
+      var volts = ((2.5 / 32768) * measurement) + 2.5;
+      var reading = (slope * volts) + intercept;
+      currentTemp = parseFloat(Math.round(reading * 100) / 100).toFixed(2);
+    }
+  }
+
+  function IEEEtoFloat(bytes) {
+    var s = bytes[3] >> 7;
+    var e = ((bytes[3] & 0x7F) << 1) | (bytes[2] >> 7);
+    var f = ((bytes[2] & 0x7F) << 16) | (bytes[1] << 8) | bytes[0];
+    if (e === 0 && f != 0) {
+      e = -126;
+      var m = 0.0;
+    } else {
+      e -= 127;
+      var m = 1.0;
+    }
+    for (var i=0; i < 23; i++) {
+      if ((f >> (23 - i)) & 0x1) {
+        m += Math.pow(2, -i);
+      }
+    }
+    return Math.pow(-1, s) * Math.pow(2, e) * m;
   }
 
   ext.getTemp = function(scale) {
@@ -75,10 +150,12 @@
       device.write(new Uint8Array(initCmd).buffer);
 
       poller = setInterval(function() {
-        device.read(function(inputData) {
-          processData(inputData);
+        device.read(function(rawData) {
+          var inputData = new Uint8Array(rawData);
+          if (inputData.length > 0)
+            initializeDevice(inputData);
         });
-      }, 50);
+      }, 10);
     });
   };
 
@@ -90,7 +167,13 @@
 
   ext._shutdown = function () {
     if (poller) poller = clearInterval(poller);
-    if (device) device.close();
+    if (device) {
+      var out = [0, CMD_SET_LED_STATE, 0, 0x04, 0, 0, 0, 0];
+      device.write(new Uint8Array(out).buffer);
+      out = [0, CMD_STOP_MEASUREMENTS, 0, 0, 0, 0, 0, 0];
+      device.write(new Uint8Array(out).buffer);
+      device.close();
+    }
     device = null;
   };
 
